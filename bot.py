aec8046d32269c4f07c3fe078cd17bffdb501da5
Test <test@example.com>
2025-09-28 14:56:19 +0500

    Add Web App

--- bot.py
+++ bot.py
@@ -1,20 +1,18 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
-from telegram.ext import ApplicationBuilder, CommandHandler
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext
 
-BOT_TOKEN = "7533731133:AAHfTRL39AHagjCMT8ZZ7Xdi0LMgCu6-ues"
+TOKEN = "7533731133:AAHfTRL39AHagjCMT8ZZ7Xdi0LMgCu6-ues"
 
-async def start(update, context):
+async def start(update: Update, context: CallbackContext.DEFAULT_TYPE):
     keyboard = [
-        [InlineKeyboardButton(
-            "Открыть Чё поесть?", 
-            web_app=WebAppInfo(url="https://cubiform-hsiu-egregiously.ngrok-free.dev")
-        )]
+        [InlineKeyboardButton("Открыть Чё поесть?", web_app=WebAppInfo(
+            url="https://Bip-boop-963.github.io/che-poest-webapp/"))]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text("Привет! Нажми на кнопку, чтобы выбрать блюдо:", reply_markup=reply_markup)
+    await update.message.reply_text("Привет! Выбери что-то вкусное:", reply_markup=reply_markup)
 
-if __name__ == "__main__":
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    print("Бот запущен...")
-    app.run_polling()
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+print("Бот запущен...")
+app.run_polling()
